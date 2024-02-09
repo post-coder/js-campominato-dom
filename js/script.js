9ecfce680187b7c44c1814430f0cac9adbf77084
post-coder <gabrielspanu.dev@gmail.com>
2024-02-09 10:34:04 +0100

    genrazione bombe e controllo click

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 /*
 
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata
-si colora di azzurro ed emetto un messaggio in console
-con il numero della cella cliccata.
-Potete realizzare lo stile che volete, 
-sin quando tutto è leggibile con facilità
+Il computer deve generare 16 numeri casuali e inserirli in un array, 
+in base al range della difficoltà prescelta 
+(se abbiamo scelto facile l'array conterrà numeri casuali da 1 a 100,
+ se invece abbiamo scelto difficile l'array dovrà contenerne da 1 a 49): 
+ questi rappreseranno le posizioni delle nostre bombe.
+Attenzione: nella stessa cella può essere posizionata al massimo una bomba, 
+perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: 
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - 
+la cella si colora di rosso e la partita termina. 
+Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba 
+o quando raggiunge il numero massimo possibile di numeri consentiti 
+(ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, 
+cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 */
 
@@ -61,6 +69,11 @@ function play() {
     gridElement.style.display = "flex";
 
 
+    // genero un array di 16 bombe
+    const bombs = getRandomNumbersArray(16, cellNumber);
+    console.log("bombe: ", bombs)
+
+
     for(let i = 0; i < cellNumber; i++) {
 
         // creo un elemento
@@ -74,7 +87,25 @@ function play() {
 
 
         // aggiungo un event listener del click ad ogni cella
-        newElement.addEventListener("click", clickManager);
+        newElement.addEventListener("click", function() {
+
+
+            // emetto un messaggio in console con il numero della cella cliccata
+            console.log(this.innerText);
+
+            // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - 
+
+            if(bombs.includes(Number(this.innerText))) {
+
+                console.log("pestata una bomba");
+
+            }
+
+            // aggiungo la classe all'elemento cliccato
+            this.classList.add("clicked");
+
+
+        });
 
     }
 
@@ -82,14 +113,49 @@ function play() {
 
 
 
-// funzione che gestisce il click di una singola cella
-function clickManager() {
-    // console.log("this dentro la funzione separata", this)
 
-    // // emetto un messaggio in console con il numero della cella cliccata
-    console.log(this.innerText);
 
-    // // aggiungo la classe all'elemento cliccato
-    this.classList.add("clicked");
+// funzione che genera un numero casuale in base ad un valore massimo dato come parametro
+// da 1 a max
+
+function getRandomNumber(max) {
+    const randomNumber = Math.floor(Math.random() * max + 1)
+    
+    return randomNumber;
+}
+
+
+
+// console.log(getRandomNumbersArray(16, 49));
+
+// data una lunghezza, genera un array con tot numeri casuali tutti diversi tra loro
+// questa funzione ha bisogno della funzione getRandomNumber
+function getRandomNumbersArray(quantity, highestNumber) {
+
+    // dichiariamo un array vuoto
+    const numbersArray = [];
+
+    let iterazioni = 0;
+
+    // avvio un ciclo while
+    do {
+        // generiamo un numero casuale
+        const newNumber = getRandomNumber(highestNumber);
+
+        if( ! numbersArray.includes(newNumber)) {
+            // solo se il numero generato non è presente nell'array
+
+            // se il numero casuale non è presente nell'array lo inseriamo
+            numbersArray.push(newNumber);
+
+        }
+
+
+        iterazioni++;
+        // continuiamo a fare tutto SINCHè la lunghezza dell'array è quella indicata
+    } while (numbersArray.length < quantity && iterazioni < 1000);
+
+
+    return numbersArray;
 
 }
